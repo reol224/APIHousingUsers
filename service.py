940540ee05b156b9ccb76e987847bc6ee6b56e87
Iulia Danilov <tmdoppelganger@gmail.com>
2023-06-09 23:44:07 -0400

    Enhanced register

--- service.py
+++ service.py
@@ -1,6 +1,6 @@
+import mysql.connector
 from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
-import mysql.connector
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
@@ -35,17 +35,33 @@ def register():
     username = data['username']
     password = data['password']
 
+    # Check if the username or password is empty
+    if not username or not password:
+        return jsonify({'message': 'Registration failed', 'error': 'Username or password cannot be empty'}), 400
+
     # Hash the password
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     conn = get_mysql_connection()
     cursor = conn.cursor()
+
     try:
+        # Check if the username already exists
+        cursor.execute("SELECT * FROM customers WHERE username = %s", (username,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            return jsonify({'message': 'Registration failed', 'error': 'Username already exists'}), 400
+
+        # Insert the new user into the database
         cursor.execute("INSERT INTO customers (username, password) VALUES (%s, %s)", (username, hashed_password))
         conn.commit()
-        return jsonify({'message': 'Registration successful'})
+
+        return jsonify({'message': 'Registration successful'}), 200
+
     except mysql.connector.Error as error:
         return jsonify({'message': 'Registration failed', 'error': str(error)}), 400
+
     finally:
         cursor.close()
         conn.close()
